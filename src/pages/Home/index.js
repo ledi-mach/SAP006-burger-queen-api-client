5ab544f8868a5b55b7b3a00268ba043801e410b6
Lediane <lediane141@gmail.com>
2021-09-03 12:15:23 -0300

    iniciando login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
-//import testImage from '../../assets/images/teste.png';
 import { useHistory } from 'react-router-dom';
 import { Button } from '../../components/Button/index.js';
+import imgBurger from '../../assets/images/figma-burger.png';
 
 
 export function Home(){
@@ -14,22 +14,22 @@ export function Home(){
     return(
 <div>
     <main>
-        <div className="burger-img">
+        <img src={imgBurger} alt="imgburger" />
         <div className="divInfo">
             <h2> LOGIN</h2>
             <form>
                 <div className="inputInfo">
-                <input type="text" placeholder="Email" />
-                <input type="password"placeholder="Digite a senha"></input>
+                <p class="labelInputs">Email</p>
+                <input type="text"/>
+                <p class="labelInputs">Senha</p>
+                <input type="password"></input>
                  </div>
                <Button btnType="submit" btnClass="orange-btn" btnText="ENTRAR" />
                </form>
+               <p className="isWorker">É funcionário?</p>
             <Button btnType="button" btnClass="red-btn" btnText="CADASTRE-SE" btnOnClick={navigateToRegister}/>
-        </div>
         </div>
     </main>
 </div>
     )
 }
-
- //<img src={testImage} alt="teste imagem" />
