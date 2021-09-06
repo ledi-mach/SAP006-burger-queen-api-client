52265d77ee6a9bb123bb4cfe314cd7cac88ec33c
Lediane <lediane141@gmail.com>
2021-09-06 17:39:53 -0300

    teste register

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import { Button } from '../../components/Button/index.js';
 import { Input } from '../../components/Input/index.js'
 import imgBurger from '../../assets/images/burger-background.png';
@@ -12,6 +13,42 @@ export function Login() {
     function navigateToRegister() {
         history.push('/register');
     }
+    function navigateToMenu() {
+        history.push('/menu');
+    }
+    
+    const apiURL = 'https://lab-api-bq.herokuapp.com';
+    const userAuth = `${apiURL}/auth`
+    const [emailLogin, setEmailLogin] = useState('');
+    const [passwordLogin, setPasswordLogin] = useState('');
+
+const logUser = () => {
+    const requestOptions = {
+        method: 'POST', 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            name:"user 1",
+            email: emailLogin,
+            password: passwordLogin,
+          }),
+    }
+    fetch(userAuth, requestOptions)
+    .then(res=> {
+        return res.json();
+    })
+
+    .then((employeeData) => {
+        localStorage.setItem('currentUserToken', employeeData.token);
+        return employeeData.token;
+    })
+    .then(()=>{
+        navigateToMenu();
+    })
+    .catch((erro)=>{
+        alert(erro)
+    })
+    console.log(requestOptions);
+}
 
     return (
         <main>
@@ -20,12 +57,16 @@ export function Login() {
             <div className="divInfo">
                 <fieldset className="formFieldsetLogin">
                     <h1 className="h1Login">LOGIN</h1>
-                    <form className="formLogin">
+                    <form>
                         <p className="labelInputs">E-mail</p>
-                        <Input inputType="email" inputClass="inputEmail" /*inputValue={emailLogin}*/ />
+                        <Input inputType="email" inputClass="inputEmail" inputValue={emailLogin} 
+                        inputOnChange={e => setEmailLogin(e.target.value)}
+                        />
                         <p className="labelInputs">Senha</p>
-                        <Input inputType="password" inputClass="inputPassword" /*inputValue={passwordLogin}*/ />
-                        <Button btnType="submit" btnClass="yellowBtn" btnText="ENTRAR" />
+                        <Input inputType="password" inputClass="inputPassword" inputValue={passwordLogin} 
+                        inputOnChange={e => setPasswordLogin(e.target.value)}
+                        />
+                        <Button btnType="submit" btnClass="yellowBtn" btnText="ENTRAR" btnOnClick={logUser} />
                         <p className="isWorker">É funcionário?</p>
                         <Button btnType="button" btnClass="redBtn" btnText="CADASTRE-SE" btnOnClick={navigateToRegister} />
                     </form>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import imgBurger from '../../assets/images/burger-background.png';
 import logo from '../../assets/images/logo.png';
 import { Button } from '../../components/Button/index.js';
@@ -6,11 +7,54 @@ import { Input } from '../../components/Input/index.js'
 
 export function Register() {
 
+
     const history = useHistory()
 
     function navigateToLogin() {
         history.push('/login');
     }
+
+    const baseURL = 'https://lab-api-bq.herokuapp.com';
+    const apiUsers = `${baseURL}/users`;
+    const [employeeEmail, setEmployeeEmail] = useState('');
+    const [employeePassword, setEmployeePassword] = useState('');
+    //precisa criar um input para a o cargo do funcionário.
+
+
+    fetch(apiUsers, {
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify ({
+            name: "user1",
+            email: employeeEmail,
+            password: employeePassword,
+            role: "kitchen",
+            restaurant: "testing-bulger",
+          
+        }),
+    }).then(res => {
+        return res.json();
+    }).catch((erro) => {
+        alert(erro)
+    })
+
+    /* const createUser = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: employeeEmail,
+                restaurant: 'test-burger',
+                password: employeePassword,
+            }),
+        }
+        fetch(apiUsers, requestOptions)
+            .then(response => response.json())
+            
+            .catch((erro) => {
+                alert(erro)
+            })
+    }  */
 
     return (
         <main>
@@ -20,15 +64,30 @@ export function Register() {
                 <Button btnType="button" btnClass="backHome" btnText="← Voltar para a Home" btnOnClick={navigateToLogin}></Button>
                 <fieldset className="formFieldsetLogin">
                     <h1 className="h1Register"> CADASTRO</h1>
+
                     <form className="formRegister">
+
                         <p className="labelInputs">Email</p>
-                        <Input btnType="email" inputClass="inputEmail" /*inputValue={emailLogin}*/ />
-                        <p class="labelInputs">Senha</p>
-                        <Input inputType="password" inputClass="inputPassword" /*inputValue={passwordLogin}*/ />
-                        <Button btnType="submit" btnClass="orangeBtn" btnText="CADASTRAR" />
+                        <Input btnType="email" inputClass="inputEmail" inputValue={employeeEmail}
+                            inputOnChange={e => setEmployeeEmail(e.target.value)}
+                            required
+                        />
+
+                        <p className="labelInputs">Senha</p>
+                        <Input inputType="password" inputClass="inputPassword" inputValue={employeePassword}
+                            inputOnChange={e => setEmployeePassword(e.target.value)}
+                            required
+                             />
+
+                        <Button btnType="submit" btnClass="orangeBtn" btnText="CADASTRAR"
+                            //btnOnClick={createUser} 
+                            />
+                    
                     </form>
+                   
                 </fieldset>
             </div>
         </main>
+       
     )
 }
